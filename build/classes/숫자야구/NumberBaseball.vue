18507114b74643c7b105f4e2f4a243bf48274339
bum2us <qka342@gmail.com>
2022-12-26 18:30:39 +0900

    node.js 설치 및 웹패키지 추가

---
+++ build/classes/숫자야구/NumberBaseball.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{result}}</h1>
+        <form v-on:submit="onSubmitForm">
+            <input ref="answer" maxlength="4" v-model="value" />
+            <button>입력</button>
+        </form>
+    <div>시도: {{}}</div>
+    </div>
+</template>
+
+<script>
+   
+</script>
+
+<style>
+
+</style>
